e9e919aa8620d7d93bdfec7117e371ad46fc30b4
unknown <paulinas@DEATHSTAR.kainos.com>
2012-09-24 00:17:20 +0200

    Added API Controller.

---
+++ AzureMVCTestApp/Controllers/HomeApiController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using AzureMVCTestApp.Models;
+
+namespace AzureMVCTestApp.Controllers
+{
+    public class HomeApiController : ApiController
+    {
+        private EmployeeDb db = new EmployeeDb();
+
+        // GET api/HomeApi
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return db.Employees.AsEnumerable();
+        }
+
+        // GET api/HomeApi/5
+        public Employee GetEmployee(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return employee;
+        }
+
+        // PUT api/HomeApi/5
+        public HttpResponseMessage PutEmployee(int id, Employee employee)
+        {
+            if (ModelState.IsValid && id == employee.EmployeeId)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // POST api/HomeApi
+        public HttpResponseMessage PostEmployee(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, employee);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = employee.EmployeeId }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // DELETE api/HomeApi/5
+        public HttpResponseMessage DeleteEmployee(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Employees.Remove(employee);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, employee);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
